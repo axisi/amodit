cb3fc9ddeb16ec08387621044a1351c41fe044f6
Grzegorz Macała <gmacala92@outlook.com>
2021-07-08 16:49:56 +0200

    Initial commit

---
+++ src/main/java/pl/cezpolska/amodit/AmoditApplication.java
@@ -0,0 +1,13 @@
+package pl.cezpolska.amodit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmoditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmoditApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/cezpolska/amodit/controller/TestController.java
@@ -0,0 +1,131 @@
+package pl.cezpolska.amodit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.Arrays;
+import java.util.Base64;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/folder/{name}")
+    public ModelAndView createFolder(@PathVariable String name) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        String prefix = "koko";
+        String secret = "aa";
+        String cipherText = name;
+        cipherText =  cipherText.replaceAll("Por21Ld", "/");
+
+        byte[] cipherData = Base64.getDecoder().decode(cipherText);
+        byte[] saltData = Arrays.copyOfRange(cipherData, 8, 16);
+
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        final byte[][] keyAndIV = GenerateKeyAndIV(32, 16, 1, saltData, secret.getBytes(StandardCharsets.UTF_8), md5);
+        SecretKeySpec key = new SecretKeySpec(keyAndIV[0], "AES");
+        IvParameterSpec iv = new IvParameterSpec(keyAndIV[1]);
+
+        byte[] encrypted = Arrays.copyOfRange(cipherData, 16, cipherData.length);
+        Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        aesCBC.init(Cipher.DECRYPT_MODE, key, iv);
+        byte[] decryptedData = aesCBC.doFinal(encrypted);
+        String decryptedText = new String(decryptedData, StandardCharsets.UTF_8);
+
+        if (decryptedText.startsWith(prefix)){
+            decryptedText = decryptedText.substring(prefix.length());
+           // System.out.println(decryptedText);
+            URL url  = new URL ("https://prod-146.westeurope.logic.azure.com:443/workflows/6270de45bbbd486c8db471eb4ca663d0/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=nDs1HD1wyvY_xzhKPfisrBxm-n7qF3ajrTXi0SQZ8zk");
+
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            String jsonInputString = "{\"Title\": \"" + decryptedText + "\"}";
+
+            try(OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            try(BufferedReader br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+
+            }
+        }
+
+
+
+        return new ModelAndView("redirect:" + "https://amodit-test.cezpolska.pl/forms/MCaselist.aspx#inbox-proc_0");
+    }
+
+    public static byte[][] GenerateKeyAndIV(int keyLength, int ivLength, int iterations, byte[] salt, byte[] password, MessageDigest md) {
+
+        int digestLength = md.getDigestLength();
+        int requiredLength = (keyLength + ivLength + digestLength - 1) / digestLength * digestLength;
+        byte[] generatedData = new byte[requiredLength];
+        int generatedLength = 0;
+
+        try {
+            md.reset();
+
+            // Repeat process until sufficient data has been generated
+            while (generatedLength < keyLength + ivLength) {
+
+                // Digest data (last digest if available, password data, salt if available)
+                if (generatedLength > 0)
+                    md.update(generatedData, generatedLength - digestLength, digestLength);
+                md.update(password);
+                if (salt != null)
+                    md.update(salt, 0, 8);
+                md.digest(generatedData, generatedLength, digestLength);
+
+                // additional rounds
+                for (int i = 1; i < iterations; i++) {
+                    md.update(generatedData, generatedLength, digestLength);
+                    md.digest(generatedData, generatedLength, digestLength);
+                }
+
+                generatedLength += digestLength;
+            }
+
+            // Copy key and IV into separate byte arrays
+            byte[][] result = new byte[2][];
+            result[0] = Arrays.copyOfRange(generatedData, 0, keyLength);
+            if (ivLength > 0)
+                result[1] = Arrays.copyOfRange(generatedData, keyLength, keyLength + ivLength);
+
+            return result;
+
+        } catch (DigestException e) {
+            throw new RuntimeException(e);
+
+        } finally {
+            // Clean out temporary data
+            Arrays.fill(generatedData, (byte)0);
+        }
+    }
+}
